62bedce75a54cc44e668ed484f665b808516854f
Felix-Red <felix.mandyme126@gmail.com>
2024-12-02 19:52:48 +0200

    added whatsapp link and icon

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
 
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav-links');
-
-    menuIcon.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('fullscreen');
+    navLinks.classList.toggle('show');
+});
 
